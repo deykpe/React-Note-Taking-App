051138cd5f989a1b8250aa6858a5896a24e9aaa0
David Pang <dpang2@uci.edu>
2019-12-20 16:12:23 -0800

    note jsx with class name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,11 @@ import React from 'react';
 function Header() {
 
     const currentYear = new Date().getFullYear();
-    return <p>
-        Copyright {currentYear}
-    </p>
+    return (
+    <footer>
+    <p>Copyright {currentYear}</p>
+    </footer>
+    )
 }
 
 export default Header;
