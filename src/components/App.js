cf67c048c733839673096c39f4926031a03e8be3
deykpe <dpang2@uci.edu>
2020-01-14 15:55:30 -0800

    added note props

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState} from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-import notes from '../notes';
 import CreateArea from './CreateArea';
-import { useState } from 'react';
+
 
 
 function App() {
@@ -19,18 +18,22 @@ function App() {
        setNotes(prevNotes => {
         return prevNotes.fillter((noteItem, index) => {
             return index !== id;
-        })
+        });
        });
    }
-}
+
 
 return (
     <div>
         <Header/>
         <CreateArea onAdd={addNote}/>
-        <Note key={1} title='Note Title' content="Note Content"/>
+        {notes.map((noteItem, index) => {
+            return (
+                <Note key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote}/>
+            );
+        })}
         <Footer/>
     </div>
-)
-
+    );
+ }
 export default App;
