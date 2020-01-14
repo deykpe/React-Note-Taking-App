84fd1db3149584ba415343915303ba49067d8db5
deykpe <dpang2@uci.edu>
2020-01-14 15:41:07 -0800

    added props

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,22 @@ import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
 import notes from '../notes';
+import CreateArea from './CreateArea';
 
 
 function App() {
-    return <div>
-        <Header/>
-        {notes.map( (noteItem) => 
-    (
-    <Note
-        key= {noteItem.key}
-        title= {noteItem.title}
-        content={noteItem.content}
-    />
-    )
-)}
-        <Footer/> 
-    </div>
+    function addNote(note) {
+        console.log(note);
+    }
 }
+
+return (
+    <div>
+        <Header/>
+        <CreateArea onAdd={addNote}/>
+        <Note key={1} title='Note Title' content="Note Content"/>
+        <Footer/>
+    </div>
+)
 
 export default App;
