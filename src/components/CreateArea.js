c72861bd2b26eea63d358d8bd7eb0c28df256d0c
deykpe <dpang2@uci.edu>
2020-01-14 15:24:15 -0800

    added spread operator function

--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
 function CreateArea () {
+
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+    function handleChange(event) {
+        const {name, value} = event.target;
+        setNote(prevNote => {
+            return {
+                ...prevNote,
+                [name]: value
+            }
+        })
+    };
     return
     <div>
         <form>
-            <input name="title" placeholder='Title' />
-            <textarea name="content" placeholder="Take a note..." rows="3" />
+            <input name="title" onChange= value={note.title} placeholder='Title' />
+            <textarea name="content" onChange= value={note.content} placeholder="Take a note..." rows="3" />
             <button>Add</button>
         </form>
     </div>
-};
+}; 
 
 export default CreateArea;
